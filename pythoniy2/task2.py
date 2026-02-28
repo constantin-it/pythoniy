7fb8d1d4363dee1cd5ee8dea1686fa1bfa3df79f
constantin-it <clankratcheta@gmail.com>
2026-02-28 20:34:41 +0800

    feat: Add solutions for season determination, molecule stoichiometry, and circle intersection problems.

--- pythoniy2/task2.py
+++ pythoniy2/task2.py
@@ -1,6 +1,15 @@
-C, H, O = input().split()
-C = int(C)
-H = int(H)
-O = int(O)
-print(min(C // 2, H // 6, O // 1)) 
+import sys
+
+sys.stdin = open('input1.txt', 'r')
+sys.stdout = open('output1.txt', 'w')
+
+while True:
+    try:
+        C, H, O = input().split()
+        C = int(C)
+        H = int(H)
+        O = int(O)
+        print(min(C // 2, H // 6, O // 1))
+    except EOFError:
+        break
         
